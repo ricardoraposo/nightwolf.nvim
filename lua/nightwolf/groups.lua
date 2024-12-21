de420c9f1327a3a41bc7b517a8cc4216e70b31a5
ricardo <ricardorbxx1@gmail.com>
2024-12-21 10:21:55 -0300

    feat: add options to customize on setup

--- lua/nightwolf/groups.lua
+++ lua/nightwolf/groups.lua
@@ -1,178 +1,204 @@
-local M = {}
+return function(p)
+  return {
+    Normal = { bg = p.background, fg = p.muted },
+    Type = { bg = '', fg = p.blue, italic = true },
+    String = { bg = '', fg = p.green },
+    Operator = { bg = '', fg = p.lightRed },
+    Number = { bg = '', fg = p.darkYellow },
+    Conditional = { bg = '', fg = p.darkPurple },
+    Statement = { bg = '', fg = p.darkPurple },
+    Error = { bg = p.red, fg = p.white },
+    Comment = { bg = '', fg = p.color9, italic = true },
+    Repeat = { bg = '', fg = p.darkPurple },
+    Function = { bg = '', fg = p.cyan, bold = true },
+    Identifier = { bg = '', fg = p.lightYellow },
+    TSCharacter = { bg = '', fg = p.lightYellow },
+    StatusLine = { bg = p.color12, fg = p.black },
+    WildMenu = { bg = p.background, fg = p.muted },
+    Pmenu = { bg = p.background, fg = p.muted },
+    PmenuSel = { bg = p.muted, fg = p.color14 },
+    PmenuThumb = { bg = p.background, fg = p.muted },
+    DiffAdd = { bg = p.color15, fg = '' },
+    DiffDelete = { bg = p.color16, fg = '' },
+    Visual = { bg = p.color17, fg = '' },
+    CursorLine = { bg = p.color17, fg = '' },
+    ColorColumn = { bg = p.color17, fg = '' },
+    SignColumn = { bg = p.background, fg = '' },
+    LineNr = { bg = '', fg = p.color18 },
+    TabLine = { bg = p.black, fg = p.color19 },
+    TabLineSel = { bg = p.muted, fg = p.color14 },
+    TabLineFill = { bg = p.black, fg = p.color19 },
+    TSPunctDelimiter = { bg = '', fg = p.muted },
+    DiagnosticDeprecated = { bg = '', fg = '', strikethrough = true },
+    Special = { bg = '', fg = p.cyan },
 
-M.groups = {
-  { "Statement", "", p.darkPurple },
-  { "Error", p.red, p.white },
-  { "Type", "", p.blue },
-  { "Type", "", "", { italic = true } },
-  { "Conditional", "", p.lightPurple },
-  { "Conditional", "", p.darkPurple },
-  { "Repeat", "", p.darkPurple },
-  { "Function", "", p.cyan, { bold = true } },
-  { "Identifier", "", p.lightYellow },
-  { "TSCharacter", "", p.lightYellow },
-  { "String", "", p.green },
-  { "Operator", "", p.lightRed },
-  { "Comment", "", p.color9, { italic = true } },
-  { "Number", "", p.darkYellow },
-  { "StatusLine", p.color12, p.black },
-  { "WildMenu", p.black, p.muted },
-  { "Pmenu", p.black, p.muted },
-  { "PmenuSel", p.muted, p.color14 },
-  { "PmenuThumb", p.black, p.muted },
-  { "DiffAdd", p.color15, "" },
-  { "DiffDelete", p.color16, "" },
-  { "Normal", p.black, p.muted },
-  { "Visual", p.color17, "" },
-  { "CursorLine", p.color17, "" },
-  { "ColorColumn", p.color17, "" },
-  { "SignColumn", p.black, "" },
-  { "LineNr", "", p.color18 },
-  { "TabLine", p.black, p.color19 },
-  { "TabLineSel", p.muted, p.color14 },
-  { "TabLineFill", p.black, p.color19 },
-  { "TSPunctDelimiter", "", p.muted },
-  { "DiagnosticDeprecated", "", "", { strikethrough = true } },
-  { "Special", "", p.cyan },
+    -- Treesitter groups
 
-  -- Treesitter groups
+    -- keyword
+    ['@keyword.import'] = { bg = '', fg = p.darkPurple },
+    ['@keyword.import.tsx'] = { bg = '', fg = p.darkPurple },
+    ['@keyword.import.rust'] = { bg = '', fg = p.lightRed },
 
-  -- keyword
-  { "@keyword.import", "", p.darkPurple },
-  { "@keyword.import.tsx", "", p.darkPurple },
-  { "@keyword.import.rust", "", p.lightRed },
+    ['@keyword'] = { bg = '', fg = p.darkPurple },
+    ['@keyword.tsx'] = { bg = '', fg = p.darkPurple },
+    ['@keyword.typescript'] = { bg = '', fg = p.blue },
+    ['@keyword.rust'] = { bg = '', fg = p.lightRed },
 
-  { "@keyword", "", p.darkPurple },
-  { "@keyword.tsx", "", p.darkPurple },
-  { "@keyword.typescript", "", p.blue },
-  { "@keyword.rust", "", p.lightRed },
+    ['@keyword.function'] = { bg = '', fg = p.blue, italic = true },
+    ['@keyword.function.tsx'] = { bg = '', fg = p.blue, italic = true },
+    ['@keyword.function.rust'] = { bg = '', fg = p.lightRed },
 
-  { "@keyword.function", "", p.blue, { italic = true } },
-  { "@keyword.function.tsx", "", p.blue, { italic = true } },
-  { "@keyword.function.rust", "", p.lightRed },
+    ['@keyword.operator'] = { bg = '', fg = p.lightRed },
+    ['@keyword.operator.tsx'] = { bg = '', fg = p.lightRed },
 
-  { "@keyword.operator", "", p.lightRed },
-  { "@keyword.operator.tsx", "", p.lightRed },
+    ['@keyword.type'] = { bg = '', fg = p.blue },
+    ['@keyword.type.typescript'] = { bg = '', fg = p.blue },
 
-  { "@keyword.type", "", p.blue },
-  { "@keyword.type.typescript", "", p.blue },
+    ['@keyword.coroutine'] = { bg = '', fg = p.darkPurple, italic = true },
+    ['@keyword.coroutine.typescript'] = { bg = '', fg = p.darkPurple, italic = true },
 
-  { "@keyword.coroutine", "", p.darkPurple, { italic = true } },
-  { "@keyword.coroutine.typescript", "", p.darkPurple, { italic = true } },
+    ['@keyword.repeat'] = { bg = '', fg = p.lightRed },
+    ['@keyword.repeat.tsx'] = { bg = '', fg = p.lightRed },
+    ['@keyword.repeat.typescript'] = { bg = '', fg = p.lightRed },
+    ['@keyword.repeat.rust'] = { bg = '', fg = p.lightRed },
 
-  { "@keyword.repeat", "", p.lightRed },
-  { "@keyword.repeat.tsx", "", p.lightRed },
-  { "@keyword.repeat.typescript", "", p.lightRed },
-  { "@keyword.repeat.rust", "", p.lightRed },
+    ['@keyword.conditional'] = { bg = '', fg = p.lightRed },
+    ['@keyword.conditional.rust'] = { bg = '', fg = p.lightRed },
 
-  { "@keyword.conditional", "", p.lightRed },
-  { "@keyword.conditional.rust", "", p.lightRed },
+    ['@keyword.modifier.rust'] = { bg = '', fg = p.lightRed },
 
-  { "@keyword.modifier.rust", "", p.lightRed },
+    -- variable
+    ['@variable'] = { bg = '', fg = p.muted },
+    ['@variable.tsx'] = { bg = '', fg = p.lightYellow },
+    ['@variable.typescript'] = { bg = '', fg = p.lightYellow },
+    ['@variable.rust'] = { bg = '', fg = p.lightYellow },
 
-  -- variable
-  { "@variable", "", p.muted },
-  { "@variable.tsx", "", p.lightYellow },
-  { "@variable.typescript", "", p.lightYellow },
-  { "@variable.rust", "", p.lightYellow },
+    ['@variable.parameter'] = { bg = '', fg = p.lightYellow },
+    ['@variable.parameter.tsx'] = { bg = '', fg = p.lightYellow },
+    ['@variable.parameter.typescript'] = { bg = '', fg = p.lightYellow },
+    ['@variable.parameter.rust'] = { bg = '', fg = p.lightYellow },
 
-  { "@variable.parameter", "", p.lightYellow },
-  { "@variable.parameter.tsx", "", p.lightYellow },
-  { "@variable.parameter.typescript", "", p.lightYellow },
-  { "@variable.parameter.rust", "", p.lightYellow },
+    ['@variable.builtin'] = { bg = '', fg = p.lightRed },
+    ['@variable.builtin.tsx'] = { bg = '', fg = p.lightRed },
+    ['@variable.builtin.typescript'] = { bg = '', fg = p.lightRed },
 
-  { "@variable.builtin", "", p.lightRed },
-  { "@variable.builtin.tsx", "", p.lightRed },
-  { "@variable.builtin.typescript", "", p.lightRed },
+    -- type
+    ['@type'] = { bg = '', fg = p.lightPurple },
+    ['@type.tsx'] = { bg = '', fg = p.lightPurple },
+    ['@type.typescript'] = { bg = '', fg = p.cyan },
+    ['@type.rust'] = { bg = '', fg = p.darkPurple },
 
-  -- type
-  { "@type", "", p.lightPurple },
-  { "@type.tsx", "", p.lightPurple },
-  { "@type.typescript", "", p.cyan },
-  { "@type.rust", "", p.darkPurple },
+    ['@type.builtin'] = { bg = '', fg = p.darkPurple },
+    ['@type.builtin.tsx'] = { bg = '', fg = p.darkPurple },
+    ['@type.builtin.typescript'] = { bg = '', fg = p.darkPurple },
+    ['@type.builtin.rust'] = { bg = '', fg = p.lightPurple },
 
-  { "@type.builtin", "", p.darkPurple },
-  { "@type.builtin.tsx", "", p.darkPurple },
-  { "@type.builtin.typescript", "", p.darkPurple },
-  { "@type.builtin.rust", "", p.lightPurple },
+    -- boolean
+    ['@boolean'] = { bg = '', fg = p.lightRed },
+    ['@boolean.tsx'] = { bg = '', fg = p.lightRed },
+    ['@boolean.rust'] = { bg = '', fg = p.lightRed },
 
-  -- boolean
-  { "@boolean", "", p.lightRed },
-  { "@boolean.tsx", "", p.lightRed },
-  { "@boolean.rust", "", p.lightRed },
+    -- operator
+    ['@operator'] = { bg = '', fg = p.muted },
+    ['@operator.tsx'] = { bg = '', fg = p.muted },
+    ['@operator.rust'] = { bg = '', fg = p.lightRed },
 
-  -- operator
-  { "@operator", "", p.muted },
-  { "@operator.tsx", "", p.muted },
-  { "@operator.rust", "", p.lightRed },
+    -- punctuation
+    ['@punctuation.delimiter'] = { bg = '', fg = p.muted },
+    ['@punctuation.bracket'] = { bg = '', fg = p.lightYellow },
+    ['@punctuation.bracket.tsx'] = { bg = '', fg = p.lightYellow },
+    ['@punctuation.bracket.lua'] = { bg = '', fg = p.lightYellow },
 
-  -- punctuation
-  { "@punctuation.delimiter", "", p.muted },
-  { "@punctuation.bracket", "", p.lightYellow },
-  { "@punctuation.bracket.tsx", "", p.lightYellow },
-  { "@punctuation.bracket.lua", "", p.lightYellow },
+    ['@punctuation.special'] = { bg = '', fg = p.lightRed },
+    ['@punctuation.special.tsx'] = { bg = '', fg = p.lightRed },
+    ['@punctuation.special.typescript'] = { bg = '', fg = p.lightRed },
 
-  { "@punctuation.special", "", p.lightRed },
-  { "@punctuation.special.tsx", "", p.lightRed },
-  { "@punctuation.special.typescript", "", p.lightRed },
+    -- constructor
+    ['@constructor'] = { bg = '', fg = p.darkPurple },
+    ['@constructor.tsx'] = { bg = '', fg = p.darkPurple },
+    ['@constructor.typescript'] = { bg = '', fg = p.blue },
+    ['@constructor.lua'] = { bg = '', fg = p.darkPurple },
 
-  -- constructor
-  { "@constructor", "", p.darkPurple },
-  { "@constructor.tsx", "", p.darkPurple },
-  { "@constructor.typescript", "", p.blue },
-  { "@constructor.lua", "", p.darkPurple },
+    -- string
+    ['@string.escape'] = { bg = '', fg = p.green },
+    ['@string.escape.tsx'] = { bg = '', fg = p.green },
+    ['@string.escape.typescript'] = { bg = '', fg = p.green },
+    ['@string.escape.rust'] = { bg = '', fg = p.green },
 
-  -- string
-  { "@string.escape", "", p.green },
-  { "@string.escape.tsx", "", p.green },
-  { "@string.escape.typescript", "", p.green },
-  { "@string.escape.rust", "", p.green },
+    -- function
+    ['@function.call'] = { bg = '', fg = p.cyan, italic = true },
+    ['@function.call.tsx'] = { bg = '', fg = p.cyan, italic = true },
+    ['@function.call.lua'] = { bg = '', fg = p.darkPurple },
 
-  -- function
-  { "@function.call", "", p.cyan, { italic = true } },
-  { "@function.call.tsx", "", p.cyan, { italic = true } },
-  { "@function.call.lua", "", p.darkPurple },
+    ['@function.method.call'] = { bg = '', fg = p.cyan, italic = true },
+    ['@function.method.call.tsx'] = { bg = '', fg = p.cyan, italic = true },
+    ['@function.method.call.typescript'] = { bg = '', fg = p.cyan, italic = true },
 
-  { "@function.method.call", "", p.cyan, { italic = true } },
-  { "@function.method.call.tsx", "", p.cyan, { italic = true } },
-  { "@function.method.call.typescript", "", p.cyan, { italic = true } },
+    -- tag
+    ['@tag'] = { bg = '', fg = p.lightRed },
+    ['@tag.tsx'] = { bg = '', fg = p.lightRed },
+    ['@tag.builtin'] = { bg = '', fg = p.lightRed },
+    ['@tag.builtin.tsx'] = { bg = '', fg = p.lightRed },
+    ['@tag.attribute'] = { bg = '', fg = p.darkPurple },
+    ['@tag.attribute.tsx'] = { bg = '', fg = p.darkPurple },
+    ['@tag.delimiter'] = { bg = '', fg = p.muted },
+    ['@tag.delimiter.tsx'] = { bg = '', fg = p.muted },
 
-  -- tag
-  { "@tag", "", p.lightRed },
-  { "@tag.tsx", "", p.lightRed },
-  { "@tag.builtin", "", p.lightRed },
-  { "@tag.builtin.tsx", "", p.lightRed },
-  { "@tag.attribute", "", p.darkPurple },
-  { "@tag.attribute.tsx", "", p.darkPurple },
-  { "@tag.delimiter", "", p.muted },
-  { "@tag.delimiter.tsx", "", p.muted },
+    -- constants
+    ['@constant'] = { bg = '', fg = p.lightRed },
+    ['@constant.builtin'] = { bg = '', fg = p.lightRed },
+    ['@constant.builtin.tsx'] = { bg = '', fg = p.lightRed },
+    ['@constant.builtin.typescript'] = { bg = '', fg = p.lightRed },
+    ['@constant.builtin.rust'] = { bg = '', fg = p.lightYellow },
 
-  -- constants
-  { "@constant", "", p.lightRed },
-  { "@constant.builtin", "", p.lightRed },
-  { "@constant.builtin.tsx", "", p.lightRed },
-  { "@constant.builtin.typescript", "", p.lightRed },
-  { "@constant.builtin.rust", "", p.lightYellow },
+    -- markup
+    ['@markup.heading.1'] = { bg = '', fg = p.muted },
+    ['@markup.heading.1.tsx'] = { bg = '', fg = p.muted },
 
-  -- markup
-  { "@markup.heading.1", "", p.muted },
-  { "@markup.heading.1.tsx", "", p.muted },
+    -- module
+    ['@module.rust'] = { bg = '', fg = p.lightPurple },
 
-  -- module
-  { "@module.rust", "", p.lightPurple },
+    -- Plugin specific
+    NvimTreeFolderName = { bg = '', fg = p.blue },
+    NvimTreeSpecialFile = { bg = '', fg = p.cyan },
+    NvimTreeOpenedFolderName = { bg = '', fg = p.cyan },
 
-  -- Plugin specific
-  { "NvimTreeFolderName", "", p.blue },
-  { "NvimTreeSpecialFile", "", p.cyan },
-  { "NvimTreeOpenedFolderName", "", p.cyan },
+    -- Telescope
+    TelescopeMatching = { bg = '', fg = p.blue },
 
-  -- Telescope
-  { "TelescopeMatching", "", p.blue },
+    -- MiniStatusLine
+    MiniStatuslineModeNormal = { bg = p.blue, fg = p.black, bold = true },
+    MiniStatuslineModeInsert = { bg = p.lightPurple, fg = p.black, bold = true },
+    MiniStatuslineModeVisual = { bg = p.lightRed, fg = p.black, bold = true },
 
-  -- MiniStatusLine
-  { "MiniStatuslineModeNormal", p.blue, p.black, { bold = true } },
-  { "MiniStatuslineModeInsert", p.lightPurple, p.black, { bold = true } },
-  { "MiniStatuslineModeVisual", p.lightRed, p.black, { bold = true } },
-}
-
-return M
+    TelescopeNormal = { link = 'Normal' },
+    NonText = { link = 'Comment' },
+    Whitespace = { link = 'Comment' },
+    Folded = { link = 'Comment' },
+    CursorLineNr = { link = 'Identifier' },
+    Macro = { link = 'Function' },
+    TSLabel = { link = 'Type' },
+    TSParameterReference = { link = 'TSParameter' },
+    TSKeyword = { link = 'Keyword' },
+    TSType = { link = 'Type' },
+    TSNamespace = { link = 'TSType' },
+    TSPunctSpecial = { link = 'TSPunctDelimiter' },
+    TSOperator = { link = 'Operator' },
+    TSConstant = { link = 'Constant' },
+    TSField = { link = 'Constant' },
+    TSFloat = { link = 'Number' },
+    TSConstBuiltin = { link = 'TSVariableBuiltin' },
+    TSParameter = { link = 'Constant' },
+    TSTag = { link = 'MyTag' },
+    TSPunctBracket = { link = 'MyTag' },
+    TSNumber = { link = 'Number' },
+    TSFunction = { link = 'Function' },
+    TSComment = { link = 'Comment' },
+    TSConditional = { link = 'Conditional' },
+    TSProperty = { link = 'TSField' },
+    TSFuncMacro = { link = 'Macro' },
+    TSRepeat = { link = 'Repeat' },
+    TSString = { link = 'String' },
+    TSTagDelimiter = { link = 'Type' },
+  }
+end
